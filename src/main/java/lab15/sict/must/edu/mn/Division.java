2d1db1939f3877c61e2209c487e7f29d055121da
Misheel Enkhtuvshin <mikaenkh@gmail.com>
2025-05-09 10:58:13 +0800

    Hotfix v1.0.1: fixed handle division by zero and improve test coverage

--- src/main/java/lab15/sict/must/edu/mn/Division.java
+++ src/main/java/lab15/sict/must/edu/mn/Division.java
@@ -2,7 +2,9 @@ package lab15.sict.must.edu.mn;
 
 public class Division {
     public double divide(double a, double b) {
-        if (b == 0) throw new IllegalArgumentException("Division by zero");
+        if (Double.compare(b, 0.0) == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero (b = " + b + ")");
+        }
         return a / b;
     }
 }
